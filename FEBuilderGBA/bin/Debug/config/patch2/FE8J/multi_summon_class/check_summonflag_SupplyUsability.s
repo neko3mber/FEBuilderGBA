f14f8996cd091ce3f372e12ef4888a8c5e1621a4
neko3mber <100101417+neko3mber@users.noreply.github.com>
2024-12-28 06:02:16 +0700

    Delete FEBuilderGBA directory

--- FEBuilderGBA/bin/Debug/config/patch2/FE8J/multi_summon_class/check_summonflag_SupplyUsability.s
+++
@@ -1,4 +0,0 @@
-.thumb
-ldrb r0, [r0, #0xF] @ramunit->status4
-lsr  r0,#0x7
-cmp r0,#0x01
--- FEBuilderGBA/bin/Debug/config/patch2/FE8U/WeaponTypeArray/Definitions.asm
+++
@@ -1,8 +0,0 @@
-
-.macro SET_FUNC name, value
-    .global \name
-    .type   \name, %function
-    .set    \name, \value
-.endm
-
-SET_FUNC WeaponTypeArray, (0x0802C7C0+1)
